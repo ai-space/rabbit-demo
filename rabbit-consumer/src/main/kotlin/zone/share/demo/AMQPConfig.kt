d274b0858c0f7f932429d26d54c3ffbf6d343786
Jiahua.zhang <i@iamee.me>
2019-02-16 10:31:52 +0800

    大概知道了，异常被提前捕获了。

--- rabbit-consumer/src/main/kotlin/zone/share/demo/AMQPConfig.kt
+++ rabbit-consumer/src/main/kotlin/zone/share/demo/AMQPConfig.kt
@@ -7,7 +7,6 @@ import org.springframework.amqp.core.Message
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.lang.RuntimeException
 
 @Configuration
 class AMQPConfig {
@@ -17,12 +16,7 @@ class AMQPConfig {
         return object : SpringAMQPMessageSource(messageConverter) {
             @RabbitListener(queues = ["zone.share.demo.consumer"])
             override fun onMessage(message: Message, channel: Channel) {
-                try {
-                    super.onMessage(message, channel)
-                } catch (ex: RuntimeException) {
-                    println("=================")
-                    ex.printStackTrace()
-                }
+                super.onMessage(message, channel)
             }
         }
     }
